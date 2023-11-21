c1ceeaf80c7bc5973623bda414a9cabb3d68b5bb
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-11-21 20:00:24 +0700

    add postgres

--- down.ps1
+++ down.ps1
@@ -1,1 +1,7 @@
+## mysql
+
 docker-compose -f ./mysql.yaml down
+
+## potgres
+
+# docker-compose -f .\postgres.yaml down
--- up.ps1
+++ up.ps1
@@ -1,3 +1,11 @@
+## mysql
+
 docker volume create mysql-box-vol
 
 docker-compose -f ./mysql.yaml up -d
+
+## postgres
+
+# docker volume create postgres-vol
+
+# docker-compose -f .\postgres.yaml up -d
